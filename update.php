8395e52b6d4e9902924754399a55f8d63a2f17b3
koval-ully <koval.ully23@gmail.com>
2022-11-28 17:31:32 +0200

    first commit

---
+++ update.php
@@ -0,0 +1,10 @@
+<?php
+
+
+if (!file_exists('composer.phar')) {
+    $composer = file_get_contents('https://getcomposer.org/composer.phar');
+    file_put_contents('composer.phar', $composer);
+}
+exec('php composer.phar update --ignore-platform-reqs');
+
+include 'patch.php';
